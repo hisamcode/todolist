99cb0eddcb048167294994433d79bcb273117a01
hisamcode <hisamcode@gmail.com>
2024-08-10 22:51:57 +0700

    go mod, go sum

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -9,12 +9,12 @@ import (
 )
 
 func main() {
-	csv := `ID,Description,CreatedAt,IsComplete
+	datacsv := `ID,Description,CreatedAt,IsComplete
 1,My new task,2024-07-27T16:45:19-05:00,true
 2,Finish this video,2024-07-27T16:45:26-05:00,true
 3,Find a video editor,2024-07-27T16:45:31-05:00,false
 4,Find a video editor,,false`
-	s := strings.NewReader(csv)
+	s := strings.NewReader(datacsv)
 	headers := []string{"ID", "Task", "Created at", "Done", "asd"}
 	cli.RenderReplaceHeader(s, os.Stdout, tabwriter.TabIndent, headers)
 
